f8020bddc4a48bf78a6e506b6b3e4ecff2d82bc7
Andrés Meza <andresproyectosx24@gmail.com>
2025-12-23 18:44:16 -0600

    env.local: actualización de credenciales de fireba

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,23 +1,19 @@
-// src/lib/firebase.js
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// 1. Importamos la autenticación
 import { getAuth } from "firebase/auth";
 
-// ⬇️ ASEGÚRATE DE QUE ESTA CONFIGURACIÓN SEA LA TUYA (NO BORRES TUS LLAVES)
+// Usamos process.env para acceder a las variables seguras
 const firebaseConfig = {
-  // ... Pega aquí tus llaves que copiaste anteriormente ...
-  // Si ya las tenías, solo deja este objeto como estaba
-   apiKey: "AIzaSyCftYklNL13qb5Gq6molS1AUkL4846wOII",
-  authDomain: "chayotex-d1b12.firebaseapp.com",
-  projectId: "chayotex-d1b12",
-  storageBucket: "chayotex-d1b12.firebasestorage.app",
-  messagingSenderId: "835094984626",
-  appId: "1:835094984626:web:0d97e21d297806300456ab"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+// Inicialización segura (Singleton)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const db = getFirestore(app);
-// 2. Exportamos la "auth" para usarla en el Login
 export const auth = getAuth(app);
